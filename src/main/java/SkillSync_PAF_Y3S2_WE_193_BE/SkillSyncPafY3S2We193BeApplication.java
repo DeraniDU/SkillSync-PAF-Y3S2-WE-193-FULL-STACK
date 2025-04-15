135fb575a99b69743e999d60bd72d59f97ce6a48
Deranindu <it22585844@my.sliit.lk>
2025-04-15 01:03:53 -0700

    Project Initialize

---
+++ src/main/java/SkillSync_PAF_Y3S2_WE_193_BE/SkillSyncPafY3S2We193BeApplication.java
@@ -0,0 +1,13 @@
+package SkillSync_PAF_Y3S2_WE_193_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSyncPafY3S2We193BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillSyncPafY3S2We193BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SkillSync_PAF_Y3S2_WE_193_BE/SkillSyncPafY3S2We193BeApplicationTests.java
@@ -0,0 +1,13 @@
+package SkillSync_PAF_Y3S2_WE_193_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSyncPafY3S2We193BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
