c5ad8457e22aa918aa53d8bc7246c1cc8e6ceb8c
samadi senavirathna <155458522+Sama20011214@users.noreply.github.com>
2025-04-27 12:31:53 +0530

    change main file

--- src/main/java/SkillSync_PAF_Y3S2_WE_193_BE/SkillSyncPafY3S2We193BeApplication.java
+++ src/main/java/SkillSync_PAF_Y3S2_WE_193_BE/SkillSyncPafY3S2We193BeApplication.java
@@ -2,12 +2,25 @@ package SkillSync_PAF_Y3S2_WE_193_BE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableTransactionManagement
 public class SkillSyncPafY3S2We193BeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SkillSyncPafY3S2We193BeApplication.class, args);
 	}
+}
 
+@RestController
+class TestController {
+	@GetMapping("/")
+	public String welcome() {
+		return "SkillSync PAF Y3S2 WE_193_BE is working!";
+	}
 }
