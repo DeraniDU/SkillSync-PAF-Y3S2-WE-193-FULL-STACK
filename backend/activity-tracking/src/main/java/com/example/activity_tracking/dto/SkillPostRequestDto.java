3f981c669f55435a487b9331f2a66539c91e124c
piyumalipalihawadana <118589931+piyumalipalihawadana@users.noreply.github.com>
2025-05-16 16:13:38 +0530

    skill post backend modify

--- backend/activity-tracking/src/main/java/com/example/activity_tracking/dto/SkillPostRequestDto.java
+++ backend/activity-tracking/src/main/java/com/example/activity_tracking/dto/SkillPostRequestDto.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+
 public class SkillPostRequestDto {
 
     @NotNull(message = "User ID is required")
